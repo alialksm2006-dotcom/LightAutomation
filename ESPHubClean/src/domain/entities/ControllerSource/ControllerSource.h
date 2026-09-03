aa68d804904e2e4c27f05192e3f470432d445050
Ali kassem <alialksm2006@gmail.com>
2026-09-04 01:50:29 +0300

    adding getter and setter to controller source id

--- ESPHubClean/src/domain/entities/ControllerSource/ControllerSource.h
+++ ESPHubClean/src/domain/entities/ControllerSource/ControllerSource.h
@@ -5,8 +5,10 @@ class ControllerSource
 private:
     int id;
     int pinNumber;
+    int controllerId;
 
 public:
+    virtual ~ControllerSource() = default;
     int getId() const
     {
         return id;
@@ -25,5 +27,14 @@ public:
     void setPinNumber(int pinNumber)
     {
         this->pinNumber = pinNumber;
+    }
+
+    int getControllerId() const
+    {
+        return controllerId;
+    }
+    void setControllerId(int controllerId)
+    {
+        this->controllerId = controllerId;
     }
 };
